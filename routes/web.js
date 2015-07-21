667dd4a834760d23b39ea77a99a02000fe33e739
Alexis Tyler <xo@wvvw.me>
2015-07-22 03:06:34 +0930

    Added seasons

--- routes/web.js
+++ routes/web.js
@@ -15,6 +15,20 @@ var express  = require('express'),
     Quality  = require('../models/Quality'),
     Network  = require('../models/Network');
 
+// Makes sure the dir exists if not it makes it.
+function ensureExists(path, mask, cb) {
+    if (typeof mask == 'function') { // allow the `mask` parameter to be optional
+        cb = mask;
+        mask = 0777;
+    }
+    fs.mkdir(path, mask, function(err) {
+        if (err) {
+            if (err.code == 'EEXIST') cb(null); // ignore the error if the folder already exists
+            else cb(err); // something else went wrong
+        } else cb(null); // successfully created folder
+    });
+}
+
 module.exports = (function() {
     var app = express.Router();
 
@@ -26,19 +40,19 @@ module.exports = (function() {
         });
     });
 
-    app.get('/poster/:mediaType/:mediaId', function(req, res){
-        if(req.params.mediaType == 'movie' || req.params.mediaType == 'show'){
-            var filePath = config.posterLocation  + '/' + req.params.mediaType +'/' + req.params.mediaId;
-            fs.stat(filePath, function(err, stat) {
-                if(err == null){
-                    res.sendFile(filePath);
-                } else {
-                    Show.findOne({_id: req.params.mediaId}, function(err, show){
-                        if(err) res.send({error: err});
-                        if(show.providers.thetvdbId){
+    app.get('/poster/show/:showId', function(req, res){
+        var filePath = config.posterLocation  + '/show/' + req.params.showId + '/show';
+        fs.stat(filePath, function(err, stat) {
+            if(err == null){
+                res.sendFile(filePath);
+            } else {
+                Show.findOne({_id: req.params.showId}, function(err, show){
+                    if(err) res.send({error: err});
+                    if(show.providers.thetvdbId){
+                        ensureExists(config.posterLocation  + '/show/' + req.params.showId + '/', 0744, function(err) {
                             tvdb.getBanners(show.providers.thetvdbId, function(error, response) {
                                 var posters = _.where(response, {BannerType: 'poster'});
-                                var file = fs.createWriteStream(config.posterLocation  + '/' + req.params.mediaType +'/' + req.params.mediaId);
+                                var file = fs.createWriteStream(filePath);
                                 var request = http.get('http://www.thetvdb.com/banners/' + posters[0].BannerPath, function(response) {
                                     response.pipe(file);
                                     file.on('finish', function() {
@@ -46,15 +60,46 @@ module.exports = (function() {
                                     });
                                 });
                             });
-                        } else {
-                            res.send({});
-                        }
-                    });
-                }
-            });
-        } else {
-            res.send('??');
-        }
+                        });
+                    } else {
+                        res.send({
+                            error: 'Not supported'
+                        });
+                    }
+                });
+            }
+        });
+    });
+
+    app.get('/poster/show/:showId/season/:seasonNumber', function(req, res){
+        var filePath = config.posterLocation  + '/show/' + req.params.showId + '/season/' + req.params.seasonNumber;
+        fs.stat(filePath, function(err, stat) {
+            if(err == null){
+                res.sendFile(filePath);
+            } else {
+                Show.findOne({_id: req.params.showId}, function(err, show){
+                    if(err) res.send({error: err});
+                    if(show.providers.thetvdbId){
+                        ensureExists(config.posterLocation  + '/show/' + req.params.showId + '/season/', 0744, function(err) {
+                            tvdb.getBanners(show.providers.thetvdbId, function(error, response) {
+                                var posters = _.where(response, {BannerType: 'season', Season: req.params.seasonNumber});
+                                var file = fs.createWriteStream(filePath);
+                                var request = http.get('http://www.thetvdb.com/banners/' + posters[0].BannerPath, function(response) {
+                                    response.pipe(file);
+                                    file.on('finish', function() {
+                                        res.sendFile(filePath);
+                                    });
+                                });
+                            });
+                        });
+                    } else {
+                        res.send({
+                            error: 'Not supported'
+                        });
+                    }
+                });
+            }
+        });
     });
 
     return app;
