8f2efe7cd1d1840147bf7f3cbf283e4cba4bb67e
Alexis Tyler <xo@wvvw.me>
2015-08-16 04:11:55 +0930

    coming soon works with banners

--- routes/poster.js
+++ routes/poster.js
@@ -38,7 +38,11 @@ module.exports = (function() {
     var app = express.Router();
 
     app.get('/show/:showId', function(req, res){
-        var filePath = config.posterLocation  + '/show/' + req.params.showId + '/show.poster';
+        if(req.query.type == 'banner'){
+            var filePath = config.posterLocation  + '/show/' + req.params.showId + '/show.banner';
+        } else {
+            var filePath = config.posterLocation  + '/show/' + req.params.showId + '/show.poster';
+        }
         fs.stat(filePath, function(err, stat) {
             if(err == null){
                 res.sendFile(filePath, {
@@ -52,7 +56,11 @@ module.exports = (function() {
                             tvdb = new thetvdb(config.apiKeys.thetvdb);
                             tvdb.getBanners(show.providers.thetvdbId, function(error, response) {
                                 if(error) res.send(error);
-                                var posters = _.where(response, {BannerType: 'poster'});
+                                if(req.query.type == 'banner'){
+                                    var posters = _.where(response, { BannerType: 'series' });
+                                } else {
+                                    var posters = _.where(response, { BannerType: 'poster' });
+                                }
                                 if(posters.length){
                                     var file = fs.createWriteStream(filePath);
                                     var request = http.get('http://www.thetvdb.com/banners/' + posters[0].BannerPath, function(response) {
@@ -136,19 +144,19 @@ module.exports = (function() {
                             tvdb.getBanners(show.providers.thetvdbId, function(error, response) {
                                 var posters = _.where(response, {BannerType: 'season', Season: req.params.seasonNumber});
                                 var file = fs.createWriteStream(filePath);
-                                if(posters.length) {
-                                    var request = http.get('http://www.thetvdb.com/banners/' + posters[0].BannerPath, function(response) {
-                                        response.pipe(file);
-                                        file.on('finish', function() {
-                                            res.sendFile(filePath);
-                                        });
-                                    });
-                                } else {
+                                // if(posters.length) {
+                                //     var request = http.get('http://www.thetvdb.com/banners/' + posters[0].BannerPath, function(response) {
+                                //         response.pipe(file);
+                                //         file.on('finish', function() {
+                                //             res.sendFile(filePath);
+                                //         });
+                                //     });
+                                // } else {
                                     filePath = config.posterLocation  + '/placeholder.png';
                                     res.sendFile(filePath, {
                                         maxAge: 30672000
                                     });
-                                }
+                                // }
                             });
                         });
                     } else {
